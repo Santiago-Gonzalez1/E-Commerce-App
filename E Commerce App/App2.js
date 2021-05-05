f6c2ba83845e773728fa4b9415e784db7225473f
Santiago Gonzalez <79683308+Santiago-Gonzalez1@users.noreply.github.com>
2021-05-05 15:17:41 -0300

    Update App2.js

--- E Commerce App/App2.js
+++ E Commerce App/App2.js
@@ -1,9 +1,12 @@
 import React from "react"
 import Catalog from "./Catalog"
+import TrendingProducts from "./Components/TrendingProducts"
+import MainCategoriesLinks from "./Components/MainCategoriesLinks"
 
  function App(){
     return(
         <div className="allbody">
+             <MainCategoriesLinks />
              <TrendingProducts />
              <Catalog />
         </div>
