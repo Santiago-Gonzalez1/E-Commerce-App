dbf7004befab42a01dffb0742735a7f3f171edc9
Santiago Gonzalez <79683308+Santiago-Gonzalez1@users.noreply.github.com>
2021-05-05 15:21:14 -0300

    Update index.js

--- E Commerce App/index.js
+++ E Commerce App/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 
 
 import App from "./App2"
+import NavBar from "./NavBar
 
+ReactDOM.render(<NavBar />, document.getElementById('navbar'));
 ReactDOM.render(<App />, document.getElementById('root'));
